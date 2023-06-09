57965c6fd21e05f8a56cb1c6aaa7140d904079c4
yuchiXiong <yuchi.xiong@foxmail.com>
2023-06-09 16:04:31 +0800

    feat: fib with cache

--- fib/src/main.rs
+++ fib/src/main.rs
@@ -1,11 +1,17 @@
 fn main() {
-    println!("fib 45 => {}", fib(45));
+    let mut cache: Vec<i64> = Vec::new();
+    println!("fib 45 => {}", fib(45, &mut cache));
 }
 
-fn fib(n: i64) -> i64 {
-    if n < 2 {
-        n
-    } else {
-        fib(n - 1) + fib(n - 2)
+fn fib(n: i64, cache: &mut Vec<i64>) -> i64 {
+    if cache.len() > n as usize {
+        return cache[n as usize];
     }
+    if n < 2 {
+        return n;
+    }
+
+    fib(n - 1, cache) + fib(n - 2, cache);
+    cache.push(result);
+    result
 }
